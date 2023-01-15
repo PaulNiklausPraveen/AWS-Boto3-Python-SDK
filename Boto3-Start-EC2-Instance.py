61910eb67f10e0a3586952d21176d41c6ff3cf6e
Paul Niklaus Praveen <62203157+PaulNiklausPraveen@users.noreply.github.com>
2023-01-15 13:44:26 +0530

    Update Boto3-Start-EC2-Instance.py

--- Boto3-Start-EC2-Instance.py
+++ Boto3-Start-EC2-Instance.py
@@ -1,3 +1,6 @@
+#This python script get Instance ID as input and start the EC2 instance and display the current status of the instance as output
+#Reference URL : https://boto3.amazonaws.com/v1/documentation/api/latest/guide/migrationec2.html
+
 import boto3
 import time
 import json
@@ -14,6 +17,6 @@ print("\nScript pause for 30 seconds,waiting for Instance to come online\n")
 time.sleep(30)
 response = ec2resource.meta.client.describe_instance_status(InstanceIds=[Instance_ID],IncludeAllInstances=True)
 output=(response['InstanceStatuses'][0])
-print (json.dumps(output, indent=2, default=str))
+print (json.dumps(output, indent=1, default=str))
 
 
