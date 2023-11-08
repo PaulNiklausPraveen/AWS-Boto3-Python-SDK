1830daa78dd98410cc46a4d701ff6c501d27a328
Paul Niklaus Praveen <paulnikpraveen@gmail.com>
2023-11-09 02:43:10 +0530

    Create S3Bucket

--- Create-S3Bucket.py
+++ Create-S3Bucket.py
@@ -1,9 +1,21 @@
 import boto3
-AWS_REGION = "us-east-2"
-resource = boto3.resource("s3", region_name=AWS_REGION)
-bucket_name = "aws-s3-bucket"
-location = {'LocationConstraint': AWS_REGION}
-bucket = resource.create_bucket(
-    Bucket=bucket_name,
-    CreateBucketConfiguration=location)
-print("Amazon S3 bucket has been created")
+import logging
+import boto3
+from botocore.exceptions import ClientError
+
+aws_access_key_id = '8d1fd22d-93ce-4766-bf93-c5ea18618edb'
+aws_secret_access_key = '8d1fd22d-93ce-4766-bf93-c5ea18618edb/l3Tl5e'
+AWS_Region = 'us-west-2'
+
+S3_Bucket = 'pythons3-bucket-20241'
+client = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key,
+                      region_name=AWS_Region)
+
+AWS_Response = client.create_bucket(
+    Bucket=S3_Bucket,
+    CreateBucketConfiguration={
+        'LocationConstraint': AWS_Region
+    }
+)
+
+print(AWS_Response)
